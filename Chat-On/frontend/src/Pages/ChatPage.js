fa9e26aea14ff44b279770fd6deff3f6ff2b2bc2
AnkitSingh-16 <98884735+AnkitSingh-16@users.noreply.github.com>
2023-04-30 13:18:56 +0530

    Add files via upload

---
+++ Chat-On/frontend/src/Pages/ChatPage.js
@@ -0,0 +1,34 @@
+import { Box } from "@chakra-ui/react";
+import { ChatState } from "../Context/ChatProvider";
+import MyChats from "../components/MyChats";
+import Chatbox from "../components/Chatbox";
+import SideDrawer from "../components/miscellaneuos/SideDrawer";
+
+import { useState } from "react";
+
+const ChatPage = () => {
+  const { user } = ChatState();
+  const [fetchAgain, setFetchAgain] = useState(false);
+
+  return (
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        w="100%"
+        h="91.5vh"
+        p="10px"
+      >
+        {user && (
+          <MyChats fetchAgain={fetchAgain}   />
+        )}
+        {user && (
+          <Chatbox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+        )}
+      </Box>
+    </div>
+  );
+};
+
+export default ChatPage;
